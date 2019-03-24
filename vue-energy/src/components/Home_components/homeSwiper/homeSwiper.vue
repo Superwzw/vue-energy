c5d880d772ca4a0efb85500ac633167b6713f045
Superwzw <wei1054495615@qq.com>
2019-03-24 11:15:05 +0800

    add user sell fnc

--- vue-energy/src/components/Home_components/homeSwiper/homeSwiper.vue
+++ vue-energy/src/components/Home_components/homeSwiper/homeSwiper.vue
@@ -1,7 +1,43 @@
 <template>
 	<div>
 		<div class="place" v-if="!this.$store.state.show">
-			<Card class="fnCard"></Card>
+			<Card class="fnCard">
+				<div class="sell-module">
+					<div class="user-info">
+						<Card class="info-content sellc">
+							<div style="text-align: center;">
+					            <img src="@/assets/logo1.png">
+				            	<h1>用户名</h1>
+				            	<h2 style="margin-top: 20px;">ID:15352330</h2>
+				            	<h4 style="color: red;margin-top: 30px;">
+				            		*请务必核对好您的基本信息，如有错误，请稍后再试
+				            	</h4>
+			          		</div>
+						</Card>
+					</div>
+					<div class="sell-detail">
+						<Card class="sell-content sellc">
+							<div style="text-align: center;">
+								<div style="display: flex;">
+							        <h2 style="margin-right: 10px;">电量:</h2>
+							        <Input size="large" prefix="ios-basket" placeholder="请输入电量,如： 100" clearable style="width: 400px;" />
+							    </div>
+								<div style="display: flex;margin-top: 20px;">
+							        <h2 style="margin-right: 10px;">单价:</h2>
+							        <Input size="large" prefix="logo-yen" placeholder="请输入单价,如：  0.5" clearable style="width: 400px;" />
+							    </div>
+								<div style="display: flex;margin-top: 20px;">
+							        <h2 style="margin-right: 10px;">密码:</h2>
+							        <Input type="password"size="large" prefix="ios-key" placeholder="请输入支付密码" clearable style="width: 400px;" />
+							    </div>
+								<div style="display: flex;margin-top:50px;">
+							        <Button type="primary">确认出售</Button>
+							    </div>							
+			          		</div>
+						</Card>
+					</div>
+				</div>
+			</Card>
 		</div>
 		<div class="homeSwiper" v-if="this.$store.state.show">
 			<div class="card-warper">
@@ -61,6 +97,29 @@
 			flex-grow:1;
 			background-color: rgba(0,0,0,0.4);
 			// border:none;
+			.sell-module
+				height:460px;
+				width:100%;
+				margin:2px auto;
+				display:flex;
+				align-items:center;
+				// border:5px solid red;
+				.user-info
+					height:90%;
+					width:30%;
+					// border:5px solid green;
+				.sell-detail
+					height:90%;
+					flex-grow:1;
+					// border:5px solid blue;
+				.sellc
+					height:100%;
+					background-color: rgba(0,0,0,0.4);
+					display:flex;
+					align-items:center;
+					justify-content:center;
+					color:white;
+					// border:none;	
 	.homeSwiper
 		height:500px;
 		width:80%;
