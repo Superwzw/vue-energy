748f2cf0e9ce241f549e8fe9a5187bcbd6a3ee3d
Superwzw <wei1054495615@qq.com>
2019-03-31 00:13:01 +0800

    sth modify

--- vue-energy/src/views/Home.vue
+++ vue-energy/src/views/Home.vue
@@ -12,7 +12,7 @@
 // import homeHeader from '@/components/Home_components/header/header.vue'
 // import homeNav from '@/components/Home_components/homeNav/homeNav.vue'
 import homeBoard from '@/components/Home_components/homeBoard/homeBoard.vue'
-import util from '@/libs/util.js'
+// import util from '@/libs/util.js'
 // import homeFooter from '@/components/Home_components/homeFooter/homeFooter.vue'
 export default {
   name: 'home',
